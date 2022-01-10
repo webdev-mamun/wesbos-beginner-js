5c23d38b4f7dddc3289f8949f60832c1564c170e
Mamunur Rashid <mamunur.m@gmail.com>
2022-01-11 02:53:29 +0600

    types

---
+++ playground/types.js
@@ -0,0 +1,16 @@
+//SNOB'N'US
+/* javascript data types
+   ***primitive =>
+        **string => Strings are written with quotes. You can use single quotes, double quotes or backtick(``).
+
+        **number=> JavaScript has only one type of numbers. Numbers can be written with, or without decimals.
+
+        **boolean => A JavaScript Boolean represents one of two values: true or false.
+
+        **undefine => A variable that has not been assigned a value is of type undefined.
+
+        **null => The value null represents the intentional absence of any object value. It is one of JavaScript's primitive values and is treated as falsy for boolean operations.
+
+        **symbol => A “symbol” represents a unique identifier.
+   ***object => Everything in javascript are object
+*/
