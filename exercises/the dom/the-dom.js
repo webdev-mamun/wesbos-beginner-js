a28d8958f69ecefee4133985169b285bf78069be
Mamunur Rashid <mamunur.m@gmail.com>
2022-03-23 03:34:38 +0600

    DOM

---
+++ exercises/the dom/the-dom.js
@@ -0,0 +1,4 @@
+const p = document.querySelector("p");
+const divs = document.querySelectorAll(".items");
+console.log(p);
+console.log(divs);
---
+++ playground/scope.js
@@ -0,0 +1,8 @@
+/* global scope => Global variables can be accessed from anywhere in a JavaScript program.
+ */
+const firstName = "mamun";
+
+/* function scope => */
+
+console.log(typeof i);
+let i = 10;
