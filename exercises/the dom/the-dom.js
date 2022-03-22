cde97e1585e97f095a4ab1f57d61ee02717e4f59
Mamunur Rashid <mamunur.m@gmail.com>
2022-03-23 03:40:22 +0600

    querySelector

--- exercises/the dom/the-dom.js
+++ exercises/the dom/the-dom.js
@@ -1,4 +1,6 @@
 const p = document.querySelector("p");
 const divs = document.querySelectorAll(".items");
+const image = document.querySelector(".items img");
 console.log(p);
 console.log(divs);
+console.log(image);
