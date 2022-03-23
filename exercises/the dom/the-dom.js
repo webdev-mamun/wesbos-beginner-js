05b121e1d2f61cf886dafef09e66afa9b436e288
Mamunur Rashid <mamunur.m@gmail.com>
2022-03-24 05:56:30 +0600

    DOM

--- exercises/the dom/the-dom.js
+++ exercises/the dom/the-dom.js
@@ -1,6 +1,21 @@
 const p = document.querySelector("p");
-const divs = document.querySelectorAll(".items");
-const image = document.querySelector(".items img");
-console.log(p);
-console.log(divs);
-console.log(image);
+const image = document.querySelector(".item img");
+const item2 = document.querySelector(".item2");
+const item2Image = item2.querySelector("img");
+const heading = document.querySelector("h2");
+
+//heading.textContent = "Mamun is cool";
+
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
+
+const pizza = document.querySelector(".pizza");
+//pizza.textContent = "🍕" + pizza.textContent + "🍕";
+//pizza.innerText = "🍕" + pizza.innerText + "🍕";
+
+//pizza.insertAdjacentText("beforeend", "🍕");
+//pizza.insertAdjacentText("afterbegin", "🍕");
+// pizza.insertAdjacentText("beforebegin", "🍕");
+// pizza.insertAdjacentText("afterend", "🍕");
