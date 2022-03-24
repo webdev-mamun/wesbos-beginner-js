25b70155d5c7902960948bf285e6aa3983a32deb
Mamunur Rashid <mamunur.m@gmail.com>
2022-03-24 06:29:30 +0600

    dom

--- exercises/the dom/the-dom.js
+++ exercises/the dom/the-dom.js
@@ -1,8 +1,8 @@
 const p = document.querySelector("p");
 const image = document.querySelector(".item img");
-const item2 = document.querySelector(".item2");
-const item2Image = item2.querySelector("img");
-const heading = document.querySelector("h2");
+//const item2 = document.querySelector(".item2");
+//const item2Image = item2.querySelector("img");
+//const heading = document.querySelector("h2");
 
 //heading.textContent = "Mamun is cool";
 
@@ -11,7 +11,7 @@ const heading = document.querySelector("h2");
 // console.log(heading.innerHTML);
 // console.log(heading.outerHTML);
 
-const pizza = document.querySelector(".pizza");
+//const pizza = document.querySelector(".pizza");
 //pizza.textContent = "🍕" + pizza.textContent + "🍕";
 //pizza.innerText = "🍕" + pizza.innerText + "🍕";
 
@@ -19,3 +19,14 @@ const pizza = document.querySelector(".pizza");
 //pizza.insertAdjacentText("afterbegin", "🍕");
 // pizza.insertAdjacentText("beforebegin", "🍕");
 // pizza.insertAdjacentText("afterend", "🍕");
+
+const pic = document.querySelector(".nice");
+pic.classList.add("cool");
+console.log(pic.classList);
+pic.classList.remove("cool");
+console.log(pic.classList);
+function toggleOpen() {
+  pic.classList.toggle("open");
+}
+
+pic.addEventListener("click", toggleOpen);
