d85b9cb7785c8f6a2f78dc95f7399f1e74e0702f
Mamunur Rashid <mamunur.m@gmail.com>
2022-03-31 07:37:03 +0600

    events

--- exercises/events/events.js
+++ exercises/events/events.js
@@ -21,33 +21,52 @@ arrow.addEventListener("click", handleArrow);
 
 const buyNows = document.querySelectorAll(".buyBtn");
 
-// buyNows.forEach(function (buyNow) {
-//   buyNow.addEventListener("click", function () {
-//     console.log("Buy Now!");
-//   });
-// });
+buyNows.forEach(function (buyNow) {
+  buyNow.addEventListener("click", function () {
+    console.log("Buy Now!");
+  });
+});
 
-// function handleBuy() {
-//   console.log("Buy now!!");
-// }
+function handleBuy() {
+  console.log("Buy now!!");
+}
 
-// buyNows.forEach(function (buyNow) {
-//   buyNow.addEventListener("click", handleBuy);
-// });
+buyNows.forEach(function (buyNow) {
+  buyNow.addEventListener("click", handleBuy);
+});
 
-// buyNows.forEach((buyNow) =>
-// buyNow.addEventListener("click", () => console.log("Buy now!!!"))
-// );
+buyNows.forEach((buyNow) =>
+  buyNow.addEventListener("click", () => console.log("Buy now!!!"))
+);
 
 const handleBuyArrow = () => console.log("Buy now!!!!");
-//buyNows.forEach((buyNow) => buyNow.addEventListener("click", handleBuyArrow));
+buyNows.forEach((buyNow) => buyNow.addEventListener("click", handleBuyArrow));
 
-// const handleBuyButtonClick = (btn) =>
-//   btn.addEventListener("click", handleArrow);
+const handleBuyButtonClick = (btn) =>
+  btn.addEventListener("click", handleArrow);
 
-// buyNows.forEach(handleBuyButtonClick);
+buyNows.forEach(handleBuyButtonClick);
 
 function handleBuyClick(button) {
   button.addEventListener("click", handleBuyArrow);
 }
 buyNows.forEach(handleBuyClick);
+
+function handleBuyButtonClick(e) {
+  e.stopPropagation();
+  const button = e.target;
+  //console.log(button.textContent);
+  //console.log(button.dataset.price);
+  //console.log("You are buying it!");
+  console.log(e.target);
+  console.log(e.currentTarget);
+  console.log(e.target === e.currentTarget);
+}
+
+buyNows.forEach(function (buyNow) {
+  buyNow.addEventListener("click", handleBuyButtonClick);
+});
+window.addEventListener("click", function (event) {
+  console.log("clicking on the window");
+  //console.log(event.target);
+});
