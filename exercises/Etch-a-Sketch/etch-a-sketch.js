7eb93939d77ed03a027a201a504a05e61b7014ec
Mamunur Rashid <mamunur.m@gmail.com>
2022-04-02 03:17:22 +0600

    Create a sketch fun game with canvas

--- exercises/Etch-a-Sketch/etch-a-sketch.js
+++ exercises/Etch-a-Sketch/etch-a-sketch.js
@@ -1,14 +1,65 @@
 const canvas = document.querySelector("#etch-a-sketch");
 const ctx = canvas.getContext("2d");
 const shakeButton = document.querySelector(".shake");
-
+const MOVE_AMOUNT = 20;
 //Setup canvas for drawing
+const { width, height } = canvas;
+let x = Math.round(Math.random() * width);
+let y = Math.round(Math.random() * height);
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
-ctx.lineWidth = 20;
+ctx.lineWidth = MOVE_AMOUNT;
+
 ctx.beginPath();
-ctx.moveTo(300, 400);
-ctx.lineTo(300, 400);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
-console.log(ctx);
+//console.log(ctx, width, height);
+function draw({ key }) {
+  hue = hue + 2;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  switch (key) {
+    case "ArrowUp":
+      y = y - MOVE_AMOUNT;
+      break;
+    case "ArrowRight":
+      x = x + MOVE_AMOUNT;
+      break;
+    case "ArrowDown":
+      y = y + MOVE_AMOUNT;
+      break;
+    case "ArrowLeft":
+      x = x - MOVE_AMOUNT;
+      break;
+  }
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+function handleKey(e) {
+  if (e.key.includes("Arrow")) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
+
+function clearCanvas() {
+  canvas.classList.add("shake");
+  ctx.clearRect(0, 0, width, height);
+  canvas.addEventListener(
+    "animationend",
+    function () {
+      console.log("the end");
+      canvas.classList.remove("shake");
+    },
+    { once: true }
+  );
+}
+
+window.addEventListener("keydown", handleKey);
+shakeButton.addEventListener("click", clearCanvas);
